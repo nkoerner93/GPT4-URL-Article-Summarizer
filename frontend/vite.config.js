185a3359028c0050afa9745ff3d643acf08c94be
Nicolas Körner <nkoerner93@gmail.com>
2023-10-05 05:00:55 +0200

    Adjust vite.config.js to have proper build outDir for vercel

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,5 +3,8 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    outDir: "dist", // This should match the distDir in your vercel.json
+  },
   plugins: [react()],
 });
